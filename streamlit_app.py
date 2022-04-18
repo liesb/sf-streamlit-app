7e30bf6013826682286c0f43663b611e26231035
Lies <lies.boelen@gmail.com>
2022-04-18 16:59:54 +0100

    Update streamlit_app.py
    Added picklist for smoothie ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 # APP
 
-streamlit.title("My parents' new healthy diner")
+streamlit.title("My Mom's new healthy diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -16,4 +16,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# picklist of fruits to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
+
